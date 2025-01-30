8aa92255e1e324e87b93f44cca2fb85078af41ed
oceaan-pendharkar <oceaanpendharkar@gmail.com>
2025-01-30 11:25:21 -0800

    Port edit and name

--- COMP4537/labs/3/server.js
+++ COMP4537/labs/3/server.js
@@ -53,6 +53,6 @@ app.get("/COMP4537/labs/3/writeFile/", (req, res) => {
 });
 
 // Start the server
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server is running on port ${PORT}`);
 });
